b86ce3d83542b9b3066bc70f9b73c2c31c56eb56
rayalva407 <rayalva407@gmail.com>
2021-01-20 17:38:22 -0700

    completes first challenge

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const inventors = [
   { first: 'Max', last: 'Planck', year:1858, passed: 1947 }
 ];
 
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethove, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Toby', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Abrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick',
+ 'Beecher, Henry', 'Beethove, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 
+ 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 
+ 'Benjamin, Walter', 'Benn, Toby', 'Bennington, Chester', 'Benson, Leana', 
+ 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 
+ 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 
+ 'Berry, Halle', 'Berry Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 
+ 'Biden, Joseph', 'Bierce, Abrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 
+ 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'
+];
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifHundreds = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
